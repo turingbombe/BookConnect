977e24a3218721e25b8bafb876acabf4904bfef1
Charlie Cook <turingbombe@gmail.com>
2016-09-07 15:13:47 -0400

    Set up routes and created welcome page and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  resources :users
+  resources :clubs
+  resources :messages
+  resources :sessions
+
+  root to: 'welcome#index'
 end
