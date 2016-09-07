f2335330b83283e65cb6d4a106e041daa9e7785d
Charlie Cook <turingbombe@gmail.com>
2016-09-07 09:43:38 -0400

    BookConnection associations made and seed file ready for use

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,3 +1,6 @@
 class Club < ApplicationRecord
-  belongs_to :book
+  has_many :messages
+  has_many :user_clubs
+  has_many :users, through: :user_clubs
+  belongs_to :book, optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_clubs
+  has_many :clubs, through: :user_clubs
+  has_many :messages, through: :clubs
+  has_many :books, through: :clubs
+  
 end
