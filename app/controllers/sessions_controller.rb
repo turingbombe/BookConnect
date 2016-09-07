5fb1b1b8f365f2e5ccf79dcc90535a4aefe72f67
marya314 <mary.boyd314@gmail.com>
2016-09-07 13:25:16 -0400

    added sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:username])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                flash[:notice] = 'username or password not correct'
+                redirect_to signin_path
+            end
+    end
 end
