370df47728dd65d7f559be05207eaa69e609a386
daniellasheva <daniellasheva@gmail.com>
2016-09-09 09:41:09 -0400

    add user id column to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909003051) do
+ActiveRecord::Schema.define(version: 20160909133947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "book_clubs", force: :cascade do |t|
-    t.integer  "book_id"
-    t.integer  "club_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -47,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160909003051) do
     t.integer  "club_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["club_id"], name: "index_messages_on_club_id", using: :btree
   end
 
