6245e687a4112c66ee386bca49412b5afb31388c
marya314 <mary.boyd314@gmail.com>
2016-09-09 13:48:11 -0400

    Added AR mailer and html email form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ApplicationMailer
 
     def welcome_email(user)
         @user = user
-        # @url =
         mail(to: @user.email, subject: 'Welcome to Book Connect')
     end
 end
