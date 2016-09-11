c9f8ebf0f1980a26a48295a6ff8d089bc72754b4
Charlie Cook <turingbombe@gmail.com>
2016-09-11 15:30:04 -0400

    Added a few things

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,9 @@ class BooksController < ApplicationController
   end
 
   def show
-    book=Book.find(params[:id])
-    test = GoogleBooks.search(book.title).first
-    @book = Book.new(title: test.title, author: test.authors, genre: test.categories, description: test.description)
+    @book=Book.find(params[:id])
+    # test = GoogleBooks.search(book.title).first
+    # @book = Book.new(title: test.title, author: test.authors, genre: test.categories, description: test.description)
 
   end
 
