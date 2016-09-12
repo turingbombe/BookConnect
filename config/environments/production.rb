344039a0a25d679d8962391677be12366bc1d1bd
Charlie Cook <turingbombe@gmail.com>
2016-09-12 16:36:20 -0400

    Added Action Cable production info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Action Cable production URI
+  config.web_socket_server_url = "wss://aqueous-crag-52137.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://aqueous-crag-52137.herokuapp.com', 'http://aqueous-crag-52137.herokuapp.com']
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
