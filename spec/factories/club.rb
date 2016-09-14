26d5f3aa53bcb082c7bdae9fc4c0ccae4120d4b8
Charlie Cook <turingbombe@gmail.com>
2016-09-14 09:38:04 -0400

    Added some Club tests

---
+++ spec/factories/club.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+  factory :club do
+
+    name {Faker::Name.name}
+    trait :active do
+      status{'active'}
+    end
+
+    trait :upcoming do
+      status{'upcoming'}
+    end
+
+    trait :archived do
+      status{'archived'}
+    end
+
+    trait :closed do
+      status{'closed'}
+    end
+
+    
+
+  end
+end
