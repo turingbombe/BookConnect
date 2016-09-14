3ddc0c2f33d14ed7791c69b1bff671e6a96adb92
daniellasheva <daniellasheva@gmail.com>
2016-09-14 18:27:01 -0400

    testing gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,9 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+require 'simplecov'
+SimpleCov.start
+
 require 'support/factory_girl'
 
 # The `.rspec` file also contains a few flags that are not defaults but that
